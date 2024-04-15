59e1310f431682e65c0c6cea4ca3fd9ddae41858
xiaojunbo <xiaojunbo@qq.com>
2024-04-15 14:26:12 +0800

    ...

--- src/@neatui/vue/src/store/myui.ts
+++ src/@neatui/vue/src/store/myui.ts
@@ -4,13 +4,11 @@ import { defineStore } from 'pinia';
 export const useMyuiStore = defineStore('myui', {
   state: () => {
     return {
-      full: 0,
+      full: localStorage.getItem('neatui_full') || 0,
       // 字号
-      fontSize: localStorage.getItem('fontSize') || 'm',
+      size: localStorage.getItem('neatui_size') || document?.documentElement?.getAttribute('size') || 'm',
       // 主题
-      theme: localStorage.getItem('theme') || 'auto',
-      // 所有主题
-      themes: [],
+      theme: localStorage.getItem('neatui_theme') || document?.documentElement?.getAttribute('theme') || 'auto',
       // 侧边
       side: 1
     };
@@ -18,19 +16,29 @@ export const useMyuiStore = defineStore('myui', {
   getters: {},
   actions: {
     // 设置全屏显示
-    async setFull(then = (res: any) => {}) {
+    async setfull(then = (res: any) => {}) {
       const full = this.full ? 0 : 1;
-      localStorage.setItem('neatui_full', `${full}`);
       this.full = full;
+      localStorage.setItem('neatui_full', `${full}`);
       then(full);
     },
     // 设置系统主题
-    async setTheme() {
-      return document.documentElement.setAttribute('theme', this.theme);
+    async settheme(theme: any = '', then = (res: any) => {}) {
+      if (theme) {
+        localStorage.setItem('neatui_theme', theme);
+        this.theme = theme;
+        document.documentElement.setAttribute('theme', theme);
+        then(theme);
+      }
     },
     // 设置系统字号
-    async setFontSize() {
-      document.documentElement.setAttribute('size', this.fontSize);
+    async setsize(size: any = '', then = (res: any) => {}) {
+      if (size) {
+        localStorage.setItem('neatui_size', size);
+        this.theme = size;
+        document.documentElement.setAttribute('size', this.size);
+        then(size);
+      }
     }
   }
 });
