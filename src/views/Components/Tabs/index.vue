53a9f2871333c034b3eeef3d73301e2c29109c03
xiaojunbo <xiaojunbo@qq.com>
2024-06-02 08:26:46 +0800

    ...

--- src/views/Components/Tabs/index.vue
+++ src/views/Components/Tabs/index.vue
@@ -1,9 +1,96 @@
 <template>
   <h1>{{ words['components.tabs'] || 'Tabs' }}</h1>
   <div class="w-ls">
-    <IFollowView>
-      <div></div>
-    </IFollowView>
+    <div>
+      <ul ui-tabs="@a xs">
+        <li class="active"><span>模板</span></li>
+        <li><span>图层</span></li>
+      </ul>
+      <ul ui-tabs="@a s">
+        <li class="active"><span>模板</span></li>
+        <li><span>图层</span></li>
+      </ul>
+      <ul ui-tabs="@a m">
+        <li class="active"><span>模板</span></li>
+        <li><span>图层</span></li>
+      </ul>
+      <ul ui-tabs="@a l">
+        <li class="active"><span>模板</span></li>
+        <li><span>图层</span></li>
+      </ul>
+      <ul ui-tabs="@a xl">
+        <li class="active"><span>模板</span></li>
+        <li><span>图层</span></li>
+      </ul>
+    </div>
+    <br />
+    <br />
+    <div class="n-sm-sub bg-fore">
+      <div>
+        <ul ui-tabs="@b xs">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+      <div>
+        <ul ui-tabs="@b s">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+      <div>
+        <ul ui-tabs="@b m">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+      <div>
+        <ul ui-tabs="@b l">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+      <div>
+        <ul ui-tabs="@b xl">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+    </div>
+    <br />
+    <br />
+    <div class="n-sm-sub bg-fore">
+      <div>
+        <ul ui-tabs="@c xs">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+      <div>
+        <ul ui-tabs="@c s">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+      <div>
+        <ul ui-tabs="@c m">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+      <div>
+        <ul ui-tabs="@c l">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+      <div>
+        <ul ui-tabs="@c xl">
+          <li class="active"><span>模板</span></li>
+          <li><span>图层</span></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
