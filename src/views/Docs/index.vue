be722088586f78ba7c5bbf790ed0846e0bd6ed04
xiaojunbo <xiaojunbo@qq.com>
2024-06-24 19:06:52 +0800

    ...

--- src/views/Docs/index.vue
+++ src/views/Docs/index.vue
@@ -3,13 +3,13 @@
     <HeadView />
     <div class="flex-block" ui-flex="row xy">
       <Sidebar class="n-sl fw-auto">
-        <span class="o-mm fs-xs mt-sl upper">{{ words['docs.introduction'] || 'Introduction' }}</span>
+        <span class="o-mm fs-xs mt-ms nb-no upper">{{ words['docs.introduction'] || 'Introduction' }}</span>
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/introduction">NeatUI</RouterLink>
-        <span class="o-mm fs-xs mt-sl upper">{{ words['docs.guides'] || 'Guides' }}</span>
+        <span class="o-mm fs-xs mt-ms nb-no upper">{{ words['docs.guides'] || 'Guides' }}</span>
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/installation">{{ words['docs.installation'] || 'Installation' }}</RouterLink>
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/use">{{ words['docs.use'] || 'Use' }}</RouterLink>
         <!-- <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/design">{{ words['docs.design'] || 'Design' }}</RouterLink> -->
-        <span class="o-mm fs-xs mt-sl upper">{{ words['docs.style'] || 'Style' }}</span>
+        <span class="o-mm fs-xs mt-ms nb-no upper">{{ words['docs.style'] || 'Style' }}</span>
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/basic">{{ words['docs.basic'] || 'Basic' }}</RouterLink>
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/color">{{ words['docs.color'] || 'Color' }}</RouterLink>
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/fontsize">{{ words['docs.fontsize'] || 'Font size' }}</RouterLink>
@@ -18,7 +18,7 @@
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/grid">{{ words['docs.grid'] || 'Grid' }}</RouterLink>
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/microcontrol">{{ words['docs.microcontrol'] || 'Microcontrol' }}</RouterLink>
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/interaction">{{ words['docs.interaction'] || 'Interaction' }}</RouterLink>
-        <span class="o-mm fs-xs mt-sl upper">{{ words['docs.version'] || 'Version' }}</span>
+        <span class="o-mm fs-xs mt-ms nb-no upper">{{ words['docs.version'] || 'Version' }}</span>
         <RouterLink active-class="co-main" class="ux-hover r-sm" to="/docs/version">{{ words['docs.change-log'] || 'Change log' }}</RouterLink>
       </Sidebar>
       <div class="flex-block bg-fore">
